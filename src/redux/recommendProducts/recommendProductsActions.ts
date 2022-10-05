69c15f6e56ffb9f65177ce63a9c0d2b8dad999e6
jordi <jordikimj@gmail.com>
2022-10-05 09:35:45 +0800

    first test3

--- src/redux/recommendProducts/recommendProductsActions.ts
+++ src/redux/recommendProducts/recommendProductsActions.ts
@@ -3,52 +3,46 @@ import { RootState } from "../store";
 import axios from "axios";
 
 export const FETCH_RECOMMEND_PRODUCTS_START = "FETCH_RECOMMEND_PRODUCTS_START";
-export const FETCH_RECOMMEND_PRODUCTS_SUCCESS =
-    "FETCH_RECOMMEND_PRODUCTS_SUCCESS";
+export const FETCH_RECOMMEND_PRODUCTS_SUCCESS = "FETCH_RECOMMEND_PRODUCTS_SUCCESS";
 export const FETCH_RECOMMEND_PRODUCTS_FAIL = "FETCH_RECOMMEND_PRODUCTS_FAIL";
 
 interface FetchRecommendProductStartAction {
-    type: typeof FETCH_RECOMMEND_PRODUCTS_START;
+  type: typeof FETCH_RECOMMEND_PRODUCTS_START;
 }
 
 interface FetchRecommendProductSuccessAction {
-    type: typeof FETCH_RECOMMEND_PRODUCTS_SUCCESS;
-    payload: any;
+  type: typeof FETCH_RECOMMEND_PRODUCTS_SUCCESS;
+  payload: any;
 }
 
 interface FetchRecommendProductFailAction {
-    type: typeof FETCH_RECOMMEND_PRODUCTS_FAIL;
-    payload: any;
+  type: typeof FETCH_RECOMMEND_PRODUCTS_FAIL;
+  payload: any;
 }
 
 export type RecommendProductAction =
-    | FetchRecommendProductStartAction
-    | FetchRecommendProductSuccessAction
-    | FetchRecommendProductFailAction;
+  | FetchRecommendProductStartAction
+  | FetchRecommendProductSuccessAction
+  | FetchRecommendProductFailAction;
 
-export const fetchRecommendProductStartActionCreator =
-    (): FetchRecommendProductStartAction => {
-        return {
-            type: FETCH_RECOMMEND_PRODUCTS_START,
-        };
-    };
-
-export const fetchRecommendProductSuccessActionCreator = (
-    data
-): FetchRecommendProductSuccessAction => {
-    return {
-        type: FETCH_RECOMMEND_PRODUCTS_SUCCESS,
-        payload: data,
-    };
+export const fetchRecommendProductStartActionCreator = (): FetchRecommendProductStartAction => {
+  return {
+    type: FETCH_RECOMMEND_PRODUCTS_START,
+  };
 };
 
-export const fetchRecommendProductFailActionCreator = (
-    error
-): FetchRecommendProductFailAction => {
-    return {
-        type: FETCH_RECOMMEND_PRODUCTS_FAIL,
-        payload: error,
-    };
+export const fetchRecommendProductSuccessActionCreator = (data): FetchRecommendProductSuccessAction => {
+  return {
+    type: FETCH_RECOMMEND_PRODUCTS_SUCCESS,
+    payload: data,
+  };
+};
+
+export const fetchRecommendProductFailActionCreator = (error): FetchRecommendProductFailAction => {
+  return {
+    type: FETCH_RECOMMEND_PRODUCTS_FAIL,
+    payload: error,
+  };
 };
 
 // thunk 可以返回一個函數，而不一定是js對象
@@ -56,17 +50,14 @@ export const fetchRecommendProductFailActionCreator = (
 // 並且可以處理異步邏輯
 // 業務邏輯可以從ui層面挪到這裡處理，代碼分層會更清晰
 export const giveMeDataActionCreator =
-    (): ThunkAction<void, RootState, unknown, RecommendProductAction> =>
-    async (dispatch, getState) => {
-        dispatch(fetchRecommendProductStartActionCreator());
-        try {
-            const { data } = await axios.get(
-                "http://123.56.149.216:8089/api/productCollections"
-            );
-            dispatch(fetchRecommendProductSuccessActionCreator(data));
-        } catch (error) {
-            if (error instanceof Error) {
-                dispatch(fetchRecommendProductFailActionCreator(error.message));
-            }
-        }
-    };
+  (): ThunkAction<void, RootState, unknown, RecommendProductAction> => async (dispatch, getState) => {
+    dispatch(fetchRecommendProductStartActionCreator());
+    try {
+      const { data } = await axios.get("https://123.56.149.216:8089/api/productCollections");
+      dispatch(fetchRecommendProductSuccessActionCreator(data));
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(fetchRecommendProductFailActionCreator(error.message));
+      }
+    }
+  };
